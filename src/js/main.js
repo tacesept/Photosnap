73aab4279202c789885faf11c3efaa939ff59895
two8 <iteinfin19@gmail.com>
2025-05-13 23:02:02 +0630

    feat(header): enhance mobile menu functionality and update navigation links

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,35 @@
 import "../css/tailwind.css";
+
+const menu = {
+  menuToggle: document.getElementById("menuToggle"),
+  iconOpen: document.getElementById("iconOpen"),
+  iconClose: document.getElementById("iconClose"),
+  mobileMenu: document.getElementById("mobile-menu"),
+  overlay: document.getElementById("overlay"),
+};
+
+function toggleMenu() {
+  const isOpen = menu.menuToggle.getAttribute("aria-expanded") === "true";
+  menu.menuToggle.setAttribute("aria-expanded", String(!isOpen));
+  menu.mobileMenu.setAttribute("aria-hidden", String(isOpen));
+  menu.iconOpen.classList.toggle("hidden");
+  menu.iconClose.classList.toggle("hidden");
+  menu.overlay.classList.toggle("hidden");
+  if (!isOpen) {
+    console.log("open");
+    menu.mobileMenu.classList.add(
+      "opacity-100",
+      "transition-all",
+      "duration-500"
+    );
+  } else {
+    console.log("close");
+    menu.mobileMenu.classList.remove(
+      "opacity-100",
+      "transition-all",
+      "duration-500"
+    );
+  }
+}
+
+menu.menuToggle.addEventListener("click", toggleMenu);

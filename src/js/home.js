5ea3c68cb7aa10157b6156d42da82d8262389313
two8 <iteinfin19@gmail.com>
2025-05-16 21:22:32 +0630

    refactor(html): clean up HTML structure by removing unnecessary line breaks and improving class attributes for consistency

--- src/js/home.js
+++ src/js/home.js
@@ -29,7 +29,7 @@ cards.forEach((card) => {
   cardElement.innerHTML = `
     <li>
       <a href="/stories.html" class="block group">
-      <div class="relative group-hover:translate-y-[-40px] transition-all duration-150"> 
+      <div class="relative group-hover:translate-y-[-40px] transition-all"> 
         <div class="absolute bottom-0 left-0 z-10 p-10 text-white font-bold w-full">
           <h3 class="text-lg leading-[25px]">${card.title}</h3>
           <p class="text-sm font-normal mt-1">${card.author}</p>
@@ -50,7 +50,7 @@ cards.forEach((card) => {
           class="absolute bottom-0 left-0 w-full h-[6px]
            border-gradient
            scale-x-0 group-hover:scale-x-100
-           origin-left transition-transform duration-150
+           origin-left transition-transform
            z-20 pointer-events-none"
         ></div>
         </div>

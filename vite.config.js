1c074e9d4c1d1b94033719c67021bc73cf65123c
two8 <iteinfin19@gmail.com>
2025-05-19 14:38:19 +0630

    feat(vite): configure Rollup input options for multiple HTML entry points to streamline the build process

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        pricing: resolve(__dirname, "pricing.html"),
+        contact: resolve(__dirname, "contact.html"),
+      },
+    },
+  },
 });
